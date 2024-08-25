67ff6453478ab28f5189d8b565b4954d9111d53e
youssefelbadry <youssefelbadry10@gmail.com>
2024-08-25 13:38:44 +0300

    commit1

--- src/componant/Articles.js
+++ src/componant/Articles.js
@@ -44,7 +44,7 @@ function Articles() {
             <td className="col-lg-2 rr">{item.title}</td>
             <td className="col-lg-2">{item.category}</td>
             <td className="col-lg-3">
-              <button style={buttonStyle}>{buttonText}</button>
+              <button className="btn" style={buttonStyle}>{buttonText}</button>
             </td>
             <td className="col-lg-3 time">{item.Time}</td>
             <td className="col-lg-2 rrr">
@@ -80,7 +80,7 @@ function Articles() {
               <div className="line"></div>
             </div>
             <div className="search">
-              <Col md>
+              <div className="col" md>
                 <FloatingLabel controlId="floatingInputGrid">
                   <div className="search-container">
                     <Form.Control
@@ -92,14 +92,14 @@ function Articles() {
                     <FontAwesomeIcon icon={faSearch} className="search-icon" />
                   </div>
                 </FloatingLabel>
-              </Col>
+              </div>
             </div>
           </div>
           {/* ================================================================= */}
           {/* SECTION2 */}
-          <Container className="mt-4">
-            <Row>
-              <Col>
+          <div className="container mt-4">
+            <div className="row">
+              <div className="col">
                 <table className="table">
                   <thead>
                     <tr>
@@ -111,32 +111,62 @@ function Articles() {
                     </tr>
                   </thead>
                 </table>
-              </Col>
-            </Row>
+              </div>
+            </div>
 
-            <Col className="push">{TdItem}</Col>
-          </Container>
+            <div className=" push">{TdItem}</div>
+          </div>
           {/* =========================================================================== */}
           <div className="next">
-            <Pagination
-              className="next1"
-              style={{
-                backgroundColor: "rgba(0, 0, 0, 0.5)", // لون الخلفية لجميع عناصر الترقيم
-                border: "none", // إزالة الحدود
-              }}
-            >
-              <Pagination.First />
-              <Pagination.Prev />
-              <Pagination.Item>{1}</Pagination.Item>
-              <Pagination.Item>{2}</Pagination.Item>
-              <Pagination.Item>{3}</Pagination.Item>
-              <Pagination.Item active>{4}</Pagination.Item>
-              <Pagination.Ellipsis />
-              <Pagination.Item>{10}</Pagination.Item>
-              <Pagination.Next />
-              <Pagination.Last />
-            </Pagination>
-          </div>
+      <ul
+        className="pagination next1"
+        style={{
+          backgroundColor: "rgba(0, 0, 0, 0.5)", // لون الخلفية لجميع عناصر الترقيم
+          border: "none", // إزالة الحدود
+        }}
+      >
+        
+        <li className="page-item">
+          <a className="page-link" href="#">
+            Last
+          </a>
+        </li>
+        <li className="page-item">
+          <a className="page-link" href="#">
+            1
+          </a>
+        </li>
+        <li className="page-item">
+          <a className="page-link" href="#">
+            2
+          </a>
+        </li>
+        <li className="page-item">
+          <a className="page-link" href="#">
+            3
+          </a>
+        </li>
+        <li className="page-item active">
+          <a className="page-link" href="#">
+            4
+          </a>
+        </li>
+        <li className="page-item">
+          <span className="page-link">...</span>
+        </li>
+        <li className="page-item">
+          <a className="page-link" href="#">
+            10
+          </a>
+        </li>
+        <li className="page-item">
+          <a className="page-link" href="#">
+            Next
+          </a>
+        </li>
+       
+      </ul>
+    </div>
         </div>
       </div>
     </div>
@@ -145,5 +175,156 @@ function Articles() {
 }
 
 export default Articles;
+
+
+
+// //IMPORTS
+// import { Container, Row, Col, Button } from "react-bootstrap";
+// import ArticlesData from "../Data/ArticlesData";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faTrashCan, faPenToSquare } from "@fortawesome/free-regular-svg-icons";
+// import React, { useState } from "react";
+// import Pagination from "react-bootstrap/Pagination";
+// import { Zoom } from "react-reveal";
+// import FloatingLabel from "react-bootstrap/FloatingLabel";
+// import Form from "react-bootstrap/Form";
+// import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
+// //================================================================================================
+// function Articles() {
+//   // DRAWITEM
+//   const TdItem = ArticlesData.map((item, index) => {
+//     let buttonStyle = {};
+//     let buttonText = "";
+
+//     if (index < 2) {
+//       buttonStyle = {
+//         backgroundColor: "#BF9B30",
+//         color: "white",
+//         borderRadius: "10px",
+//         border: "none",
+//         padding: "5px 15px",
+//       };
+//       buttonText = "Published";
+//     } else if (index < 6) {
+//       buttonStyle = {
+//         backgroundColor: "gray",
+//         color: "white",
+//         borderRadius: "10px",
+//         border: "none",
+//         padding: "5px 30px",
+//       };
+//       buttonText = "Draft";
+//     }
+
+//     return (
+//       <table className="table" key={index}>
+//         <tbody>
+//           <tr >
+//             <td className="col-lg-2 rr">{item.title}</td>
+//             <td className="col-lg-2">{item.category}</td>
+//             <td className="col-lg-3">
+//               <button style={buttonStyle}>{buttonText}</button>
+//             </td>
+//             <td className="col-lg-3 time">{item.Time}</td>
+//             <td className="col-lg-2 rrr">
+//               <div className="icons rrr">
+//                 <FontAwesomeIcon
+//                   icon={faTrashCan}
+//                   className="col-lg-6"
+//                   style={{ cursor: "pointer" }}
+//                 />
+//                 <FontAwesomeIcon
+//                   icon={faPenToSquare}
+//                   className="col-lg-6"
+//                   style={{ cursor: "pointer" }}
+//                 />
+//               </div>
+//             </td>
+//           </tr>
+//         </tbody>
+//       </table>
+//     );
+//   });
+//   //===============================================================================
+
+//   return (
+//     <Zoom>
+//     <div>
+//       {/* SECTION 1 */}
+//       <div className="all-project col-lg-12">
+//         <div className="opicity">
+//           <div className="hheader">
+//             <div className="line-hight col-9">
+//               <h2 style={{ margin: "20px 30px" }}>Articles</h2>
+//               <div className="line"></div>
+//             </div>
+//             <div className="search">
+//               <Col md>
+//                 <FloatingLabel controlId="floatingInputGrid">
+//                   <div className="search-container">
+//                     <Form.Control
+//                       className="form"
+//                       type="text"
+//                       placeholder="Search"
+//                       style={{ backgroundColor: "gray", border:'0'}}
+//                     />
+//                     <FontAwesomeIcon icon={faSearch} className="search-icon" />
+//                   </div>
+//                 </FloatingLabel>
+//               </Col>
+//             </div>
+//           </div>
+//           {/* ================================================================= */}
+//           {/* SECTION2 */}
+//           <Container className="mt-4">
+//             <Row>
+//               <Col>
+//                 <table className="table">
+//                   <thead>
+//                     <tr>
+//                       <td className="col-lg-2">Title</td>
+//                       <td className="col-lg-2">Category</td>
+//                       <td className="col-lg-3">Status</td>
+//                       <td className="col-lg-3">Date & Time</td>
+//                       <td className="col-lg-2">Delete & Edit</td>
+//                     </tr>
+//                   </thead>
+//                 </table>
+//               </Col>
+//             </Row>
+
+//             <Col className="push">{TdItem}</Col>
+//           </Container>
+//           {/* =========================================================================== */}
+//           <div className="next">
+//             <Pagination
+//               className="next1"
+//               style={{
+//                 backgroundColor: "rgba(0, 0, 0, 0.5)", // لون الخلفية لجميع عناصر الترقيم
+//                 border: "none", // إزالة الحدود
+//               }}
+//             >
+//               <Pagination.First />
+//               <Pagination.Prev />
+//               <Pagination.Item>{1}</Pagination.Item>
+//               <Pagination.Item>{2}</Pagination.Item>
+//               <Pagination.Item>{3}</Pagination.Item>
+//               <Pagination.Item active>{4}</Pagination.Item>
+//               <Pagination.Ellipsis />
+//               <Pagination.Item>{10}</Pagination.Item>
+//               <Pagination.Next />
+//               <Pagination.Last />
+//             </Pagination>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//     </Zoom>
+//   );
+// }
+
+// export default Articles;
+
 
 
--- src/componant/Jops.js
+++ src/componant/Jops.js
@@ -22,7 +22,7 @@ function Jops() {
               <td className="col-lg-3">{item.Email} </td>
               <td className="col-lg-2 time">{item.phone}</td>
               <td className="col-lg-3 rrr">
-               <Button style={{backgroundColor:"#BF9B30", border:'0'}}>DOWNLOAD CV</Button>
+               <button className="btn" style={{backgroundColor:"#BF9B30", border:'0'}}>DOWNLOAD CV</button>
               </td>
             </tr>
           </tbody>
@@ -43,7 +43,7 @@ return (
           <div className="line"></div>
         </div>
         <div className="search">
-          <Col md>
+          <div className="col" md>
             <FloatingLabel controlId="floatingInputGrid">
               <div className="search-container">
                 <Form.Control
@@ -55,14 +55,14 @@ return (
                 <FontAwesomeIcon icon={faSearch} className="search-icon" />
               </div>
             </FloatingLabel>
-          </Col>
+          </div>
         </div>
       </div>
       {/* ================================================================= */}
       {/* SECTION2 */}
-      <Container className="mt-4">
-        <Row>
-          <Col>
+      <div  className="container mt-4">
+        <div className="row">
+          <div className="col">
             <table className="table">
               <thead>
                 <tr>
@@ -74,32 +74,62 @@ return (
                 </tr>
               </thead>
             </table>
-          </Col>
-        </Row>
+          </div>
+        </div>
 
-        <Col className="push">{jItem}</Col>
-      </Container>
+        <div className="col push">{jItem}</div>
+      </div>
       {/* =========================================================================== */}
       <div className="next">
-        <Pagination
-          className="next1"
-          style={{
-            backgroundColor: "rgba(0, 0, 0, 0.5)", // لون الخلفية لجميع عناصر الترقيم
-            border: "none", // إزالة الحدود
-          }}
-        >
-          <Pagination.First />
-          <Pagination.Prev />
-          <Pagination.Item>{1}</Pagination.Item>
-          <Pagination.Item>{2}</Pagination.Item>
-          <Pagination.Item>{3}</Pagination.Item>
-          <Pagination.Item active>{4}</Pagination.Item>
-          <Pagination.Ellipsis />
-          <Pagination.Item>{10}</Pagination.Item>
-          <Pagination.Next />
-          <Pagination.Last />
-        </Pagination>
-      </div>
+      <ul
+        className="pagination next1"
+        style={{
+          backgroundColor: "rgba(0, 0, 0, 0.5)", // لون الخلفية لجميع عناصر الترقيم
+          border: "none", // إزالة الحدود
+        }}
+      >
+        
+        <li className="page-item">
+          <a className="page-link" href="#">
+            Last
+          </a>
+        </li>
+        <li className="page-item">
+          <a className="page-link" href="#">
+            1
+          </a>
+        </li>
+        <li className="page-item">
+          <a className="page-link" href="#">
+            2
+          </a>
+        </li>
+        <li className="page-item">
+          <a className="page-link" href="#">
+            3
+          </a>
+        </li>
+        <li className="page-item active">
+          <a className="page-link" href="#">
+            4
+          </a>
+        </li>
+        <li className="page-item">
+          <span className="page-link">...</span>
+        </li>
+        <li className="page-item">
+          <a className="page-link" href="#">
+            10
+          </a>
+        </li>
+        <li className="page-item">
+          <a className="page-link" href="#">
+            Next
+          </a>
+        </li>
+       
+      </ul>
+    </div>
     </div>
   </div>
 </div>
@@ -109,8 +139,128 @@ return (
 
 export default Jops
 
-
-
-
 //================================================================================================
+
+
+// //IMPORTS
+// import { Container, Row, Col, Button } from "react-bootstrap";
+// import JospsData from "../Data/JopsData";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import {React,  useState } from "react";
+// import Pagination from "react-bootstrap/Pagination";
+// import { Zoom } from "react-reveal";
+// import FloatingLabel from "react-bootstrap/FloatingLabel";
+// import Form from "react-bootstrap/Form";
+// import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
+
+// function Jops() {
+//   const jItem = JospsData.map((item) => {
+    
+//       return (
+//         <table className="table" >
+//           <tbody>
+//             <tr >
+//               <td className="col-lg-3 rr">{item.title}</td>
+//               <td className="col-lg-1">{item.Experiance}</td>
+//               <td className="col-lg-3">{item.Email} </td>
+//               <td className="col-lg-2 time">{item.phone}</td>
+//               <td className="col-lg-3 rrr">
+//                <Button style={{backgroundColor:"#BF9B30", border:'0'}}>DOWNLOAD CV</Button>
+//               </td>
+//             </tr>
+//           </tbody>
+//         </table>
+//       );
+//     });
+// //===============================================================================
+
+// return (
+//   <Zoom>
+// <div>
+//   {/* SECTION 1 */}
+//   <div className="all-project col-lg-12">
+//     <div className="opicity">
+//       <div className="hheader">
+//         <div className="line-hight col-9">
+//           <h2 style={{ margin: "20px 30px" }}>Applications "For "Job" at" company name"</h2>
+//           <div className="line"></div>
+//         </div>
+//         <div className="search">
+//           <Col md>
+//             <FloatingLabel controlId="floatingInputGrid">
+//               <div className="search-container">
+//                 <Form.Control
+//                   className="form"
+//                   type="text"
+//                   placeholder="Search"
+//                   style={{ backgroundColor: "gray", border:'0'}}
+//                 />
+//                 <FontAwesomeIcon icon={faSearch} className="search-icon" />
+//               </div>
+//             </FloatingLabel>
+//           </Col>
+//         </div>
+//       </div>
+//       {/* ================================================================= */}
+//       {/* SECTION2 */}
+//       <Container className="mt-4">
+//         <Row>
+//           <Col>
+//             <table className="table">
+//               <thead>
+//                 <tr>
+//                   <td className="col-lg-3">Title</td>
+//                   <td className="col-lg-">Experiance</td>
+//                   <td className="col-lg-3">Email</td>
+//                   <td className="col-lg-2">Mobile Number</td>
+//                   <td className="col-lg-3">Uploud</td>
+//                 </tr>
+//               </thead>
+//             </table>
+//           </Col>
+//         </Row>
+
+//         <Col className="push">{jItem}</Col>
+//       </Container>
+//       {/* =========================================================================== */}
+//       <div className="next">
+//         <Pagination
+//           className="next1"
+//           style={{
+//             backgroundColor: "rgba(0, 0, 0, 0.5)", // لون الخلفية لجميع عناصر الترقيم
+//             border: "none", // إزالة الحدود
+//           }}
+//         >
+//           <Pagination.First />
+//           <Pagination.Prev />
+//           <Pagination.Item>{1}</Pagination.Item>
+//           <Pagination.Item>{2}</Pagination.Item>
+//           <Pagination.Item>{3}</Pagination.Item>
+//           <Pagination.Item active>{4}</Pagination.Item>
+//           <Pagination.Ellipsis />
+//           <Pagination.Item>{10}</Pagination.Item>
+//           <Pagination.Next />
+//           <Pagination.Last />
+//         </Pagination>
+//       </div>
+//     </div>
+//   </div>
+// </div>
+// </Zoom>
+//   )
+// }
+
+// export default Jops
+
+
+
+
+// //================================================================================================
+
+
+
+
+
+
 
